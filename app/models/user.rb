a8835d7dacdd02915e9988f54570387e43d92bc0
y-ohta <y-ohta@administraotr-no-MacBook-Air.local>
2016-02-16 12:38:37 +0900

    twitterログインを作成した

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,28 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :lockable, :timeoutable, :omniauthable, omniauth_providers: [:twitter, :facebook]
+         :omniauthable, omniauth_providers: [:twitter, :facebook]
+
+  def self.from_omniauth(auth)
+   where(provider: auth["provider"], uid: auth["uid"]).first_or_create do |user|
+     user.provider = auth["provider"]
+     user.uid = auth["uid"]
+     user.username = auth["info"]["nickname"]
+   end
+  end
+
+  def self.new_with_session(params, session)
+   if session["devise.user_attributes"]
+       new(session["devise.user_attributes"], without_protection: true) do |user|
+         user.attributes = params
+         user.valid?
+       end
+   else
+       super
+   end
+  end
+
+  def password_required?
+    super && provider.blank?
+  end
 end
